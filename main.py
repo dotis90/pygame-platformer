c6dfc9c6674b3db308adb42f5eceef2c90ff0351
dotis90 <dotis90@gmail.com>
2021-10-18 18:41:05 -0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import pygame
+import sys
+from settings import *
+from level import Level
+
+pygame.init()
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+level = Level(level_map, screen)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    screen.fill('black')
+    level.run()
+
+    pygame.display.update()
+    clock.tick(60)
---
+++ settings.py
@@ -0,0 +1,16 @@
+level_map = [
+    '                            ',
+    '                            ',
+    '                            ',
+    ' XX    XXX            XX    ',
+    ' XX                         ',
+    ' XXXX        XX          XX ',
+    ' XXXX      XX               ',
+    ' XX    X  XXX     XX  XX    ',
+    '       X XXXX     XX  XXX   ',
+    '    XXXX XXXXXX   XX  XXXX  ',
+    'XXXXXXXX XXXXXX   XX  XXXX  ']
+
+tile_size = 64
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = len(level_map) * tile_size
