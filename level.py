a137f5160e0daeac2fe0f1ff5faa910eb565dc2b
dotis90 <dotis90@gmail.com>
2021-10-18 19:13:09 -0400

    Added camera movement

--- level.py
+++ level.py
@@ -1,6 +1,6 @@
 import pygame
 from tiles import Tile
-from settings import tile_size
+from settings import tile_size, SCREEN_WIDTH, SCREEN_HEIGHT
 from player import Player
 
 
@@ -26,8 +26,24 @@ class Level:
                     player_sprite = Player((x, y))
                     self.player.add(player_sprite)
 
+    def scroll_x(self):
+        player = self.player.sprite
+        player_x = player.rect.centerx
+        direction_x = player.direction.x
+
+        if player_x < SCREEN_WIDTH / 4 and direction_x < 0:
+            self.world_shift = 8
+            player.speed = 0
+        elif player_x > SCREEN_WIDTH - (SCREEN_WIDTH / 4) and direction_x > 0:
+            self.world_shift = -8
+            player.speed = 0
+        else:
+            self.world_shift = 0
+            player.speed = 8
+
     def run(self):
         self.tiles.update(self.world_shift)
         self.tiles.draw(self.display_surface)
         self.player.update()
         self.player.draw(self.display_surface)
+        self.scroll_x()
