e68270b6f802c0909c9aa481cfa047d829845111
dotis90 <dotis90@gmail.com>
2021-10-30 12:57:00 -0400

    Removed unused files, added new assets

--- levels/tilesets/crates.tsx
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="crates" tilewidth="58" tileheight="42" tilecount="1" columns="0">
- <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0">
-  <image width="58" height="42" source="../../graphics/environment/crate.png"/>
- </tile>
-</tileset>
---
+++ levels/tilesets/wasteland.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="wasteland" tilewidth="16" tileheight="16" tilecount="512" columns="32">
+ <image source="../../graphics/environment/Wasteland.png" width="512" height="256"/>
+</tileset>
